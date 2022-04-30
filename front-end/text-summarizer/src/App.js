4b15ce731c2bdf25ff438d181d7f2754817cdc03
saipranay47 <saipranayadepu07@gmail.com>
2022-04-30 12:24:27 +0530

    template

--- front-end/text-summarizer/src/App.js
+++ front-end/text-summarizer/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { FileUpload } from './FileUpload';
+import { useState } from 'react';
 
 function App() {
+
+  const [fileData, setFileData] = useState({
+    fileName: '',
+    fileType: ''
+  });
+
+  const [inputType, setInputType] = useState('1');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <select onChange={(e) => setInputType(e.target.value)}>
+        <option value="1">Text</option>
+        <option value="2">Image</option>
+        <option value="3">PDF</option>
+        <option value="4">Doc</option>
+      </select>
+
+
+      <label htmlFor="url">URL</label>
+      <input type="text" id='url' name='url' />
+
+      <label>Image</label>
+      <FileUpload />
+
+      {/* <button onClick={() => {
+        if (inputType === '1') {
+          axios.get(`http://127.0.0.1:5000/api/summarize?type={1,2,3,4}&link=OBJECT_LOCATION */}
+
     </div>
   );
 }
