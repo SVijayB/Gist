4221f7b22879ca6a8cb99ffc4df9b8a7ba7f243c
engineerscodes <practicalengineers05@gmail.com>
2022-11-04 21:01:13 +0530

    checking mime type

--- src/routes/Gmail/gmail_api.py
+++ src/routes/Gmail/gmail_api.py
@@ -3,18 +3,20 @@ from __future__ import print_function
 import base64
 import json
 import os
-
 from bs4 import BeautifulSoup
 from flask import request, Blueprint, redirect
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from src.components import summarizer
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 gmail_bp = Blueprint("gmail", __name__, url_prefix="/gist")
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 flow = InstalledAppFlow.from_client_secrets_file(
-    'client_secret.json', SCOPES,
+    CURR_DIR + '\\client_secret.json', SCOPES,
     redirect_uri="http://127.0.0.1:5000/api/gist/callback"
 )
 
@@ -40,6 +42,35 @@ def user_redirect():
         for msg in messages:
             gmail = service.users().messages().get(userId='me', id=msg['id']).execute()
             payload = gmail['payload']
+            try:
+                if payload["mimeType"] == 'text/html':
+                    body = payload["body"]
+                    data = body['data']
+                    data = data.replace("-", "+").replace("_", "/")
+                    decoded_data = base64.b64decode(data)
+                    soup = BeautifulSoup(decoded_data, "lxml")
+                    text = soup.text
+                    print(text)
+
+                else:
+                    pass
+                i = i + 1
+            except Exception:
+                pass
+
+        # print(responses)
+
+        return json.dumps(responses)
+    except Exception as e:
+        print(e)
+        return "callback!!!!!!!!!!!!!!!!!"
+
+
+"""
+
+        for msg in messages:
+            gmail = service.users().messages().get(userId='me', id=msg['id']).execute()
+            payload = gmail['payload']
             try :
                 part = payload.get("parts")[0]
                 data = part['body']['data']
@@ -47,13 +78,14 @@ def user_redirect():
                 decoded_data = base64.b64decode(data)
                 soup = BeautifulSoup(decoded_data, "lxml")
                 body = soup.body()
-                responses[i] = str(body)
+                responses[i] = str(body).replace('\n'," ").replace("\r"," ")
+                result = summarizer.summarize(responses[i])
+                print(result)
                 i = i+1
             except Exception:
                 pass
 
-        print(responses)
+        #print(responses)
+
         return json.dumps(responses)
-    except Exception as e:
-        print(e)
-        return "callback!!!!!!!!!!!!!!!!!"
+"""
