204e99265b88576162a949b932ad916a90a60638
svijayb <svijayb.dev@gmail.com>
2022-11-10 23:12:19 +0530

    Fixed dotenv not working

--- src/routes/gist/gist_main_api.py
+++ src/routes/gist/gist_main_api.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, Blueprint
 import json
 import requests
 import os
+from dotenv import load_dotenv
+from os import getenv
 
 gist_bp = Blueprint("Gist", __name__, url_prefix="/gist")
 
@@ -13,20 +15,20 @@ def gist():
     # Component to send the article to the summarizer using summarize_api (Just call local API using requests)
     # Create MongoDB account and save articles. Format mentioned as below.
     # [{"title": "title of article", "summary": "summary generated", "url": "url of news article", "date_and_time": "value"}, {repeat}]
-    #.format is not working check once
-    url="https://content.guardianapis.com/search?api-key=a30ecd9a-662c-431b-9d69-ded54b8c51f7"
-    #.format(os.getenv("Guardian_API_Key"))
+    # .format is not working check once
+    load_dotenv()
+    Guardian_API_Key = getenv("Guardian_API_Key")
+    url = f"https://content.guardianapis.com/search?api-key={Guardian_API_Key}"
     response = requests.get(url)
-    data= response.json()["response"]
+    data = response.json()["response"]
     result = []
     for article in data["results"]:
-        article={
-            "title":article["webTitle"],
-            "summary":"will generate",
-            "url":article["webUrl"],
-            "date_and_time":article["webPublicationDate"]
+        article = {
+            "title": article["webTitle"],
+            "summary": "will generate",
+            "url": article["webUrl"],
+            "date_and_time": article["webPublicationDate"],
         }
         result.append(article)
 
     return jsonify(result)
-
