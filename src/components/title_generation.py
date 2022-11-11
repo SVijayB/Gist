c4e1fea784a953a81dbdfdb7b2d8dbe74aae0498
svijayb <svijayb.dev@gmail.com>
2022-11-12 02:33:06 +0530

    Fixed regex summary text showing up (finally!).

--- src/components/title_generation.py
+++ src/components/title_generation.py
@@ -6,16 +6,20 @@ from os import getenv
 
 
 def title_generation(data):
-    print("[!] Server logs: Title generation has started")
-    text = data["content"]
-    API_URL = "https://api-inference.huggingface.co/models/Michau/t5-base-en-generate-headline"
-    text = {"inputs": f"{text}"}
-    load_dotenv()
-    HF_TOKEN = getenv("HF_TOKEN")
-    headers = {"Authorization": f"Bearer {HF_TOKEN}"}
-    response = requests.post(API_URL, headers=headers, json=text)
-    result = json.loads(response.content.decode("utf-8"))
-    result = result[0]["generated_text"]
-    print("[!] Server logs: Title generation completed")
+    try:
+        print("[!] Server logs: Title generation has started")
+        text = data["content"]
+        API_URL = "https://api-inference.huggingface.co/models/Michau/t5-base-en-generate-headline"
+        text = {"inputs": f"{text}"}
+        load_dotenv()
+        HF_TOKEN = getenv("HF_TOKEN")
+        headers = {"Authorization": f"Bearer {HF_TOKEN}"}
+        response = requests.post(API_URL, headers=headers, json=text)
+        result = json.loads(response.content.decode("utf-8"))
+        result = result[0]["generated_text"]
+        print("[!] Server logs: Title generation completed")
+    except:
+        data["title"] = "Title Generation Failed! Please try again later."
+        print("[!] Server logs: Title generation failed")
     data["title"] = result
     return data
