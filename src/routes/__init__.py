a20c97e8414aea94d1c6ac93d6c9ce7b1ccd00b7
svijayb <svijayb.dev@gmail.com>
2022-11-24 14:08:57 +0530

    Updated data endpoint for gist

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from src.routes.extraction import extraction_api
 from src.routes.summarizer import summarize_api
 from src.routes.categorizer import categorizer_api
-from src.routes.gist import gist_main_api,gist_app_api
+from src.routes.gist import gist_main_api, gist_data_api
 from src.routes.gmail import gmail_api
 
 api_blueprint = Blueprint("API", __name__, url_prefix="/api/")
@@ -11,8 +11,7 @@ api_blueprint.register_blueprint(summarize_api.summarize_bp)
 api_blueprint.register_blueprint(categorizer_api.category_bp)
 api_blueprint.register_blueprint(gmail_api.gmail_bp)
 api_blueprint.register_blueprint(gist_main_api.gist_bp)
-api_blueprint.register_blueprint(gist_app_api.gist_app_bp)
-
+api_blueprint.register_blueprint(gist_data_api.gist_data_bp)
 
 
 @api_blueprint.route("/", methods=["GET"])
