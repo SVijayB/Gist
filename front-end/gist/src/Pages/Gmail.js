0f6430b11169c2dc20032c000ac4dbb98beba217
engineerscodes <practicalengineers05@gmail.com>
2022-12-21 00:30:06 +0530

    Query String Added

--- front-end/gist/src/Pages/Gmail.js
+++ front-end/gist/src/Pages/Gmail.js
@@ -12,6 +12,7 @@ function Gmail(props){
   
   const [modal,setmodal] = useState(false);
   const [login,setlogin] = useState(false);
+  const [Emails,SetEmails] = useState(5)
   const [btndisabled,setdisabled] = useState(false);
   
 
@@ -35,13 +36,17 @@ function Gmail(props){
     
   }
 
+  const OnDropdownHandlers = (event) =>{
+      SetEmails(event.target.value)
+  }
+
   const closeModal =()=>{
     setlogin(false);
     setmodal(false);
   }
   
   const post_token = (data) =>{
-      axios.post(`http://127.0.0.1:5000/api/gmail/callback`,data).then((res)=>{
+      axios.post(`http://127.0.0.1:5000/api/gmail/callback?NoEmails=${Emails}`,data).then((res)=>{
         console.log(res.data)
           setmodal(true);
           setlogin(true);
@@ -64,6 +69,14 @@ return (
         <div className="Gmail_container" id="Top">
           <div className="info_gmail_text">
             Login into your Gmail Account
+          </div>
+          <div className="GmailDropdown">
+            <label htmlFor="No_of_mail">Number of Gmails to Summarize</label>
+                <select name="No_of_mail" id="No_of_mail" defaultValue={5}onChange={OnDropdownHandlers}>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                </select>
           </div>
             <GoogleLogin 
                       clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
